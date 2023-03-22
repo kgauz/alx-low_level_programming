d92d6aee96901d6c28e66d0415ade9442b548e13
kgauz <kgaugelomatshela@gmail.com>
2023-03-22 08:15:24 -0700

    5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -8,17 +8,17 @@ int print_sign(int n)
 {
 	if (n > 0)
 	{
+		_putchar('+');
 		return (1);
-		printf("+\n");
 	}
 	else if (n == 0)
 	{
+		_putchar('0');
 		return (0);
-		printf("0\n");
 	}
 	else
 	{
+		_putchar('-');
 		return (-1);
-		printf("-\n");
 	}
 }
