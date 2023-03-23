4ba91fc55b1edf3ae5d7559e541d77c81813b772
kgauz <kgaugelomatshela@gmail.com>
2023-03-22 18:28:02 -0700

    11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -15,7 +15,7 @@ void print_to_98(int n)
 		{
 			if (i != 98)
 			{
-				printf("%d, ", i);
+				printf("%d,", i);
 			}
 			else if (i == 98)
 			{
@@ -29,7 +29,7 @@ void print_to_98(int n)
 		{
 			if (j != 98)
 			{
-				printf("%d, ', j);
+				printf("%d,", j);
 			}
 			else if (j == 98)
 			{
