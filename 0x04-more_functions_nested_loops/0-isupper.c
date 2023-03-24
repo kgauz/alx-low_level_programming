ac8aa0f535b7294a1cda1f25f074cba0ffdfd4da
kgauz <kgaugelomatshela@gmail.com>
2023-03-24 11:37:32 -0700

    0-isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * Description - return 1 if c is uppercase
+ * return 0 otherwise
+ */
+int _isupper(int c)
+{
+	if (isupper(c))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
