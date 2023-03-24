5e730aafdb267c6c121319e3c4daca02a05cdba1
kgauz <kgaugelomatshela@gmail.com>
2023-03-24 16:24:01 -0700

    10-main.c

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * Description - print triangle
+ */
+void print_triangle(int size)
+{
+	if ( size == 0 || size < 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		_putchar('#');
+	}
+}
