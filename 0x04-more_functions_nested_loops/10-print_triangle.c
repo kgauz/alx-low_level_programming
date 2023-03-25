623abf3895dd07d22eccee7060abbf733505a4d0
kgauz <kgaugelomatshela@gmail.com>
2023-03-24 18:33:41 -0700

    10-print_triangle.c

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -4,15 +4,25 @@
  */
 void print_triangle(int size)
 {
-	int r = 0;
-
-	while (r != size)
-	{
-		_putchar('#');
-		r++;
-	}
-	if ( size == 0 || size < 0)
+	if (size <=  0)
 	{
 		_putchar('\n');
+	}
+	else
+	{
+		int i, j;
+
+		for ( i = 1; i < size; i++)
+		{
+			for ( j = i; j < size; j++)
+			{
+				_putchar(' ');
+			}
+			for (j = 1; j <= i; j++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
 	}
 }
