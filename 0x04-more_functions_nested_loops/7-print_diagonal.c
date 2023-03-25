90947a0f57963f77b4f568bc53eb849ea6051548
kgauz <kgaugelomatshela@gmail.com>
2023-03-24 18:05:21 -0700

    7-print_diagonal.c

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -4,14 +4,28 @@
  */
 void print_diagonal(int n)
 {
-	if (n == n * '_')
-	{
-		_putchar('_');
-	}
-	_putchar('\n');
-	if (n == 0 || n < 0 )
+	if (n <= 0)
 	{
 		_putchar('\n');
 	}
-}
-	  
+	else
+	{
+		int i, j;
+
+		for (i = 0; i < n; i++)
+		{
+			for (j = 0; j < n; j++)
+			{
+				if (j == i)
+				{
+					_putchar('\\');
+				}
+				else if (j < i)
+				{
+					_putchar('');
+				}
+			}
+			_putchar('\n');
+		}
+	}
+}	  
