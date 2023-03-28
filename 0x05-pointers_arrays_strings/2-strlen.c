2f99ad3caea1bea305bd7f64d31421df9229b149
kgauz <kgaugelomatshela@gmail.com>
2023-03-28 03:55:14 -0700

    2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,11 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _strlen - calculate the length of a string
+ * @s: receive the character value from the main
+ * Return: length of the string
+ */
+int _strlen(char *s)
+{
+	return (strlen(*s));
+}
