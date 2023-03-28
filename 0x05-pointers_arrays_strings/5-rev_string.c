613527e1ff81c457b06a65f8de5861618a0af4b8
kgauz <kgaugelomatshela@gmail.com>
2023-03-28 09:43:03 -0700

    5-rev_string.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,9 +1,21 @@
 #include "main.h"
+#include <string.h>
 /**
  * rev_string - reverse a string
  * @s: receive string value from the main
  */
 void rev_string(char *s)
 {
-	strrev(s);
+	int i, len, temp, left, right;
+
+	len = strlen(s);
+	right = len - 1;
+	left = 0;
+	for (i = left; i < right; i++)
+	{
+		temp = s[i];
+		s[i] = s[right];
+		s[right] = temp;
+		right--;
+	}
 }
