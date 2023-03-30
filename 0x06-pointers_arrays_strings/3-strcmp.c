18130482b2fa2535f4001935f17421da2098942a
kgauz <kgaugelomatshela@gmail.com>
2023-03-30 09:11:06 -0700

    3-strcmp.c

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _strcmp - compares two strings
+ * @s1: compare it with s2
+ * @s2: compare it with s1
+ * Return: int
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int comp;
+
+	comp = strcmp(s1, s2);
+	return (comp);
+}
