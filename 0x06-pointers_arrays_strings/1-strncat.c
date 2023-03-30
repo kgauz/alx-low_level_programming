f87208e39a9ad23467d902fe89da631241b8b66f
kgauz <kgaugelomatshela@gmail.com>
2023-03-30 07:09:23 -0700

    1-strncat.c

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -12,7 +12,7 @@ char *_strncat(char *dest, char *src, int n)
 	int i, length;
 
 	length = strlen(dest);
-	for (i = 0; i < n; i++)
+	for (i = 0; i < n && src[i] != '\0'; i++)
 	{
 		dest[length + i] = src[i];
 	}
