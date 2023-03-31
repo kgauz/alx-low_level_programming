a9d906106fc657f3f69825e354063b496d6a3b00
kgauz <kgaugelomatshela@gmail.com>
2023-03-31 09:54:19 -0700

    101-print_number.c

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -6,6 +6,18 @@
  */
 void print_number(int n)
 {
-	_putchar();
+	unsigned int a;
+
+	a = n;
+	if ( n < 0)
+	{
+		_putchar('-');
+		a = -a;
+	}
+	if ( a / 10 != 0)
+	{
+		print_number(a/10);
+	}
+	_putchar( (a % 10) + '0');
 }
 
