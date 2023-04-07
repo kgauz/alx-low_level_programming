98fbe88e50b39e5eb34503d80936fb300c1fd342
kgauz <kgaugelomatshela@gmail.com>
2023-04-06 20:47:44 -0700

    101-wildcmp.c

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,50 @@
+#include "main.h"
+
+int check(char *s, int m, int length);
+int strlen_recursion(char *s);
+
+/**
+ * is_palindrome - return one if string is a palindrome
+ * @s: string
+ */
+int is_palindrome(char *s)
+{
+	if (*s == 0)
+	{
+		return (1);
+	}
+	return(check(s, 0, strlen_recursion(s)));
+}
+
+/**
+ * strlen_recursion - return length
+ * @s: string
+ * Return: length
+ */
+int strlen_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		return (0);
+	}
+	return (1 + strlen_recursion(s + 1 ));
+}
+
+/**
+ * check - check palindrome
+ * @s: string
+ * @m: iterator
+ * @length: length
+ */
+int check(char *s, int m, int length);
+{
+	if (*(s + m) != *(s + length - 1 ))
+	{
+		return (0);
+	}
+	if (m >= length)
+	{
+		return (1);
+	}
+	return (check(s, m + 1, length - 1));
+}
